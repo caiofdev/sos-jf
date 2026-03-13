7eb09aab8bd9234539f8b4af7891ed720c886bae
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2026-03-13 00:05:13 -0300

    feat: move emergency numbers to navigation bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,16 +14,15 @@ export default function Header() {
             Encontre os pontos de coleta de doações e ajude quem mais precisa
           </p>
         </div>
-      </div>
-
-      <div className={styles.emergencyBar}>
-        <span className={styles.service}><span>🚨 Defesa Civil:</span><a href="tel:199">199</a></span>
-        <span className={styles.divider}>|</span>
-        <span className={styles.service}><span>🚒 Bombeiros:</span><a href="tel:193">193</a></span>
-        <span className={styles.divider}>|</span>
-        <span className={styles.service}><span>🚑 SAMU:</span><a href="tel:192">192</a></span>
-        <span className={styles.divider}>|</span>
-        <span className={styles.service}><span>🚓 Polícia Militar:</span><a href="tel:190">190</a></span>
+        <div className={styles.numbers}>
+          <span className={styles.service}><span>🚨 Defesa Civil:</span><a href="tel:199">199</a></span>
+          <span className={styles.divider}>|</span>
+          <span className={styles.service}><span>🚒 Bombeiros:</span><a href="tel:193">193</a></span>
+          <span className={styles.divider}>|</span>
+          <span className={styles.service}><span>🚑 SAMU:</span><a href="tel:192">192</a></span>
+          <span className={styles.divider}>|</span>
+          <span className={styles.service}><span>🚓 Polícia Militar:</span><a href="tel:190">190</a></span>
+        </div>
       </div>
     </header>
   )
